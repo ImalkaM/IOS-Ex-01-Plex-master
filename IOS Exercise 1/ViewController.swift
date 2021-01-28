e783a0a3d20126c04836bcc97217fa85b7592cd0
Imalka <Imalka.emedia@gmail.com>
2021-01-28 13:55:53 +0530

    Landscape support

---
+++ IOS Exercise 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS Exercise 1
+//
+//  Created by Imalka Muthukumara on 2021-01-23.
+//  Copyright © 2021 Imalka Muthukumara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
